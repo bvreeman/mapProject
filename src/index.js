01e563945f6014808ede131e5d987fb386b8f1ea
bvreeman <bvreeman@gmail.com>
2019-03-11 21:57:52 -0500

    changed the index.js back to remove router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App />
-    </Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
